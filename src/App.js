9eba8c8c515122e5d774460ce6fd02a58afb8b74
joabsondesouza <joabsondesouza@hotmail.com>
2022-08-25 10:17:57 -0300

    change code for new repository

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './styles/global.css';
+import Home from './pages/Home';
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+const divStyle = {
+  display: 'flex',
+  height: '100vh',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+function Home() {
+  return (
+    <div style={divStyle}>
+      <h1>Hello Galera</h1>
+    </div>
+  );
+}
+
+export default Home;
